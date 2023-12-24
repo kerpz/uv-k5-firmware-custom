68fad99b7b03cb737edb8bb7a3215f726cce2329
Philip Bordado <kerpz@yahoo.com>
2023-12-24 23:09:32 +0800

    updates

--- driver/bk4819.c
+++ driver/bk4819.c
@@ -697,7 +697,7 @@ void BK4819_SetFilterBandwidth(const BK4819_FilterBandwidth_t Bandwidth, const b
 
 void BK4819_SetupDeviation(const uint16_t dev)
 {
-	uint16_t hw_dev = ((dev > 0 ? 1 : 0) << 12) | (dev << 0);;
+	uint16_t hw_dev = ((dev > 0 ? 1 : 0) << 12) | (dev << 0);
 	BK4819_WriteRegister(BK4819_REG_40, hw_dev);
 }
 
